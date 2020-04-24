bee58e0bb4edd59e8bd1ab1b4b21a3b75d5dc5db
Forrest Bingham <bingham_5@live.com>
2020-04-24 11:55:01 -0600

    FInished MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,45 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        #FIFO, 
 
+        #Adding to the Ring Buffer. 
+        #If nothing in storage, add to head.
+        if self.storage.length == 0:
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+
+        # if storage is at max and current is not at head, remove from head
+        elif self.storage.length == self.capacity and self.current is not self.storage.head:
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+
+        #If storage is at max and current is at head, remove from tail
+        elif self.storage.length == self.capacity:
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+        #else: puts it in the middle
+        else:
+            self.storage.add_to_tail(item)
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+
+        #Loop through the array 
+        while len(list_buffer_contents) < self.storage.length:
+            #Add the current value
+            list_buffer_contents.append(self.current.value)
+
+            #If there is a next, set next node to current
+            if self.current.next:
+                self.current = self.current.next
+            #End of list
+            else:
+                self.current = self.storage.head
+            
 
         return list_buffer_contents
 
